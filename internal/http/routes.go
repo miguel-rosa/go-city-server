ce4c748f1da0064421f515136a3a471321fb8933
Miguel Gonçalves <miguel.goncalves@corebiz.ag>
2024-02-13 12:48:35 -0300

    fix: remove wip logs and routes

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -1,34 +1,17 @@
 package http
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type person struct {
-	name     string
-	quantity int
-}
-
-func respTest(ctx *gin.Context) {
-
-	// response.quantity = 2
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"name": "miguel",
-	})
-}
-
 func SetRoutes(g *gin.Engine) {
 	g.GET("/posts", GetAll)
-	g.GET("/post/:id", GetPost)
-	g.POST("/post", PostPosts)
-	g.POST("/test", respTest)
+	g.GET("/posts/:id", GetPost)
+	g.POST("/posts", PostPosts)
 
 	g.GET("/health", func(c *gin.Context) {
-		fmt.Printf("OK!")
 		c.Status(http.StatusOK)
 	})
 }
