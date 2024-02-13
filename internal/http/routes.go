f2c87fd478da40a678baed367dbc0e616803ccab
Miguel Gonçalves <miguel.goncalves@corebiz.ag>
2024-02-13 12:21:22 -0300

    fix: wip test post handler

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -6,10 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type person struct {
+	name     string
+	quantity int
+}
+
+func respTest(ctx *gin.Context) {
+
+	// response.quantity = 2
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"name": "miguel",
+	})
+}
+
 func SetRoutes(g *gin.Engine) {
 	g.GET("/posts", GetAll)
 	g.GET("/post/:id", GetPost)
 	g.POST("/post", PostPosts)
+	g.POST("/test", respTest)
 
 	g.GET("/health", func(c *gin.Context) {
 		c.Status(http.StatusOK)
